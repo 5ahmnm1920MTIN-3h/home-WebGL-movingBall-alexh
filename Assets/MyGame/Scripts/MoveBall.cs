f43c8147c3b11f0470bd8be8e38f55d3b5483671
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2020-01-12 12:10:20 +0100

    add MoveBall script which moves ball to 4 different positions randomly with bool which controlls if the ball can move to the same position

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    [SerializeField] private Transform[] randomPositions;
+    [SerializeField] private Transform defaultPosition;
+    [SerializeField] private Transform ball;
+    [SerializeField] private bool alwaysDifferentPos = true;
+
+    void Start()
+    {
+        ball.position = defaultPosition.position;
+    }
+
+    public void moveBall()
+    {
+        Vector3 oldPos;
+        Vector3 newPos;
+
+        do
+        {
+            oldPos = ball.position;
+            newPos = randomPositions[Random.Range(0, randomPositions.Length)].position;
+            ball.position = newPos;
+        }
+        // check condition after executing the above code
+        while (oldPos == newPos && alwaysDifferentPos);
+    }
+}
